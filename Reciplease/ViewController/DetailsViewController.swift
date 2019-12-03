a20160ad5a0ec8dfe78eb9471bea54160303e26b
Samahir Adi <samahir@pandaguidesme.com>
2019-12-03 15:35:10 +0100

    Change tabBar icons and color

--- Reciplease/ViewController/DetailsViewController.swift
+++ Reciplease/ViewController/DetailsViewController.swift
@@ -19,6 +19,7 @@ class DetailsViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
+    
     @IBOutlet weak var ingredientsTableView: UITableView!
     
     @IBOutlet weak var getDirectionButtonView: UIButton!
@@ -28,8 +29,8 @@ class DetailsViewController: UIViewController {
     @IBAction func getDirections(_ sender: Any) {
         guard let recipe = recipe else {return}
         if let url = URL(string: recipe.url!) {
-           UIApplication.shared.open(url)
-       }
+            UIApplication.shared.open(url)
+        }
     }
     
     // - MARK: Variables
@@ -44,25 +45,36 @@ class DetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let recipe = recipe {
-        self.titleLabel.text = recipe.label
-        if let imageUrlString = recipe.image,
-            let imageUrl = URL(string: imageUrlString) {
-            self.imageView.kf.setImage(with: imageUrl)
-        }
+        configurateViews()
         setupTableView()
-        }
-
+        setupRoundedCorners()
     }
     
-    func setupTableView() {
+    
+    
+    
+    private func setupTableView() {
         self.ingredientsTableView.delegate = self
         self.ingredientsTableView.dataSource = self
         let nib = UINib(nibName: DetailsViewController.kCellId, bundle: nil)
         self.ingredientsTableView.register(nib, forCellReuseIdentifier: DetailsViewController.kCellId)
     }
     
-
+    private func setupRoundedCorners() {
+        imageView.layer.cornerRadius = imageView.frame.height / 10
+        getDirectionButtonView.layer.cornerRadius = getDirectionButtonView.frame.height / 8
+    }
+    
+    private func configurateViews() {
+        if let recipe = recipe {
+            self.titleLabel.text = recipe.label?.uppercased()
+            if let imageUrlString = recipe.image,
+                let imageUrl = URL(string: imageUrlString) {
+                self.imageView.kf.setImage(with: imageUrl)
+            }
+        }
+    }
+    
 }
 
 extension DetailsViewController: UITableViewDelegate, UITableViewDataSource {
@@ -73,14 +85,14 @@ extension DetailsViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
+        return 75
     }
-
-
+    
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: DetailsViewController.kCellId, for: indexPath) as! IngredientsTableViewCell
-         if let recipe = recipe {
+        if let recipe = recipe {
             cell.ingredientLabel.text = recipe.ingredients?[indexPath.row].text
         }
         return cell
